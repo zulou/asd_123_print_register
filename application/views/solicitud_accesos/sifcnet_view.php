66c2b7672eac7d96621a9e122992c192840f83d7
OPERADOR <OPERADOR@GTI-UGS-07>
2015-05-13 19:18:34 -0500

    test ini124

--- application/views/solicitud_accesos/sifcnet_view.php
+++ application/views/solicitud_accesos/sifcnet_view.php
@@ -283,10 +283,177 @@
 
 
 
+
+
+<div id="solicitud_impresion">
+    <table border="0" cellspacing="0" cellpadding="0" width="633">
+        <tbody>
+            <tr>
+                <td width="87">
+                    <p align="center">
+                        <strong></strong>
+                    </p>
+                </td>
+                <td width="452">
+                    <p align="center">
+                        <strong>FORMATO 01-AU</strong>
+                    </p>
+                    <p align="center">
+                        <strong>FORMATO DE SOLICITUD DE CREACIÓN O CAMBIO DE PERMISOS DE CUENTA DE USUARIO SIFCNet</strong>
+                    </p>
+                </td>
+                <td width="93">
+                    <p align="center">
+                        <strong></strong>
+                    </p>
+                    <p>
+                        <strong>Nº</strong>
+                    </p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="input-prepend">
+        <table border="2px">       
+            <tbody>
+                <tr>
+                    <td colspan="2">
+                        <span class="add-on">Nombres:</span>
+                        <input class="span8" id="prependedInput" type="text" placeholder="Username">
+                    </td>  
+
+
+                </tr>              
+
+                <tr>
+                    <td>
+
+                        <span class="add-on">  Email:</span>
+                        <input class="span5" id="prependedInput" type="text" placeholder="Username">
+                    </td>  
+                    <td>
+                        <span class="add-on">Fecha:</span>
+                        <input class="span2" id="prependedInput" type="text" placeholder="Username">
+                    </td>  
+                </tr>
+
+                <tr>
+                    <td>
+                        <span class="add-on">Cargo:</span>
+                        <input class="span5" id="prependedInput" type="text" placeholder="Username">
+                    </td>  
+                    <td>
+                        <span class="add-on">Area:</span>
+                        <input class="span2" id="prependedInput" type="text" placeholder="Username">
+                    </td>  
+                </tr>
+                <tr>
+                    <td>
+                        <span class="add-on">OFICINA/PAC:</span>
+                        <input class="span5" id="prependedInput" type="text" placeholder="Username">
+                    </td>  
+                    <td>
+                        <span class="add-on">DNI:</span>
+                        <input class="span2" id="prependedInput" type="text" placeholder="Username">
+                    </td>  
+                </tr>
+
+
+            </tbody>
+        </table>    
+    </div>
+
+    <p>
+        <small>*Una vez lleno este espacio, entréguelo a su jefe inmediato superior o Gerente del área (en caso de no encontrarse el Jefe Superior) para que lo llene y envíe al responsable de Administración de Cuentas para que se encargue de procesar su solicitud y entregarle su usuario y contraseña a su cuenta de Correo Electrónico Personal.</small>
+    </p>
+
+
+
+    <table border="2px">
+        <tr>   
+            <td colspan="2">
+                <div class="input-prepend">
+                    <span class="add-on">Nombres:</span>
+                    <input class="span8" id="prependedInput" type="text" placeholder="Username">
+                </div>
+            </td>   
+        </tr>
+        <tr>
+            <td>
+                <table>
+                    <tr>
+                        <td>algo1</td>
+                    </tr>
+                    <tr>
+                        <td>algo2</td>
+                    </tr>
+                    <tr>
+                        <td>algo3</td>
+                    </tr>
+                </table>
+            </td>
+            <td>
+                <table>
+                    <tr>
+                        <td>algo22</td>
+                    </tr>
+                    <tr>
+                        <td>algo22</td>
+                    </tr>
+                    <tr>
+                        <td>algo22</td>
+                    </tr>
+                </table>
+            </td>     
+
+        </tr>
+
+        <tr>
+            <td>
+            </td>
+            <td>
+                <table>
+                    <tr>
+                        <td>algo22</td>
+                    </tr>
+                    <tr>
+                        <td>algo22</td>
+                    </tr>
+                    <tr>
+                        <td>algo22</td>
+                    </tr>
+                </table>
+            </td>
+
+        </tr>
+    </table> 
+    <h6>Espacio llenado por el Gerente del Área o Jefe Inmediato Superior</h6>
+
+    <table border="2px">    
+        <tr>
+            <td>
+
+                <div class="input-prepend">
+                    <span class="add-on">Nombres:</span>
+                    <input class="span5" id="prependedInput" type="text" placeholder="Username">
+                </div>
+            </td>
+            <td>
+                <p align="center"><small>JEFE DE LA UNIDAD DE GESTIÓN DE SISTEMAS Y APLICACIONES: SELLO Y FIRMA</small></p>
+            </td>        
+        </tr>
+
+    </table>
+
+
+</div>
+
+
+
                                                             <script>
                                                                 $(function () {
 
-
+                                                                    $("#solicitud_impresion").hide();
 
 
                                                                     $("#hasta").datepicker({
