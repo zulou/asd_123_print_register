3227cab786dc95849d379270ff12f68fc7e5b4d0
OPERADOR <OPERADOR@GTI-UGS-07>
2015-07-13 18:56:14 -0500

    test ini128

--- application/views/login/about.php
+++ application/views/login/about.php
@@ -1,6 +1,5 @@
         
-</div>
-</section>
+
 
 <!-- About Section -->
 <section class="success" id="about">
@@ -11,40 +10,46 @@
                 <hr class="star-light">
             </div>
         </div>
-        <div class="widget-body" style="border-color: #0134c5" style="border: 3px">
-            <div class="span12">
-                <form class="form-horizontal">
-                <div class="control-group">
-                    <label class="control-label" for="inputEmail">Email</label>
-                    <div class="controls">
-                        <input type="text" id="inputEmail" placeholder="Email">
-                    </div>
-                </div>
-                <div class="control-group">
-                    <label class="control-label" for="inputPassword">Password</label>
-                    <div class="controls">
-                        <input type="password" id="inputPassword" placeholder="Password">
-                    </div>
-                </div>
-                <div class="control-group">
-                    <div class="controls">
-                        <label class="checkbox">
-                            <input type="checkbox"> Remember me
-                        </label>
-                        <button type="submit" class="btn">Sign in</button>
-                    </div>
-                </div>
-            </form>
-            </div>    
+        <div class="widget-body">
+            <form id="form-proveedor-datos" class="form-horizontal no-margin center-align-text text-center"  >     
 
-        </div>
-        <div class="row">
+                <div class="row">
+                    
 
-            <div class="col-lg-8 col-lg-offset-2 text-center">
-                <a href="#" class="btn btn-lg btn-outline">
-                    <i class="fa fa-download"></i> Download Theme
-                </a>
-            </div>
+                    <div class="col-lg-12 col-lg-offset-2 text-center">
+                        <div class="row-fluid">
+                            <div class="span8">
+                                <div class="widget">
+
+                                    <div class="widget-body">
+
+
+                                        <div class="control-group ruc">
+                                            <label class="control-label">Usuario:</label>
+                                            <div class="controls">
+                                                <input class="input-block-level" type="text" title="Ingrese usuario" name="Usuario" id="usuario" required='true' >
+                                            </div>
+                                        </div>
+
+                                        <div class="control-group">
+                                            <label class="control-label">Password:</label>
+                                            <div class="controls">
+                                                <input class="input-block-level" type="password" name="password" id="password"  required="true">
+                                            </div>
+                                        </div>
+
+
+                                        <!--</form>-->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <a href="#" class="btn btn-lg btn-outline">
+                            <i class="fa fa-download"></i> Download Theme
+                        </a>
+                    </div>
+                </div>
         </div>
     </div>
 </section>
